6d2bf1084898b9b3c12f26fb6b31a5fed71e594f
BekaHabesha <bereketdm1984@gmail.com>
2023-09-01 16:48:55 -0700

    0-whatsmyname.c.

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+
+int _putchar(char c);
+void _puts(char *s);
+int main (int argc, char *argv[]);
+
+#endif
