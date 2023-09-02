ffd5a408bebd034ef6253cc30bdbc7d73b82a65d
BekaHabesha <bereketdm1984@gmail.com>
2023-09-01 18:45:25 -0700

    3-mul.c .

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,19 +9,17 @@
 *
 * Return: Always zero
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int n1 = 0, n2 = 0;
 
-if (argc == 3)
+if (argc != 3)
 {
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
+printf("Error\n");
+return (1);
 }
 else
 {
-printf("Error\n");
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
 }
-return (1);
 }
