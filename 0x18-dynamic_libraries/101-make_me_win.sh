6230e476ed518c7e51be629e4a415866b307cc54
BekaHabesha <bereketdm1984@gmail.com>
2023-11-13 13:47:18 -0800

    README.md file 0x18. C - Dynamic libraries -README.md file Authored by Bereket Dereje

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /tmp/ https://raw.githubusercontent.com/BekaHabesha/alx-low_level_programming/main/0x18-dynamic_libraries/lib_beki_hacker.so
-export LD_PRELOAD=/tmp/lib_beki_hacker.so
